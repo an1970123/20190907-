fa652806ab12d125b498a7fe2821813767166895
michaelchen <namieamuro1978@outlook.com>
2019-09-07 15:13:49 +0800

    Initial Commit

---
+++ 20190907骰子/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  20190907骰子
+//
+//  Created by Michael Chen on 2019/9/7.
+//  Copyright © 2019 Michael Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
