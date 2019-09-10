300ee408fd1f0147cfb03cc29a66356186abce54
michaelchen <namieamuro1978@outlook.com>
2019-09-10 18:28:08 +0800

    20190910

--- 20190907骰子/ViewController.swift
+++ 20190907骰子/ViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var dices: [UIImageView]!
+    let imageNames = ["one", "two" , "three" , "five" , "six"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake{
+            for imageView in dices{
+                imageView.image = UIImage(named: imageNames.randomElement()!)
+            }
+        }
+    }
+    @IBAction func sankaku(_ sender: Any) {
+        for imageView in dices{
+            imageView.image =
+                UIImage(named:imageNames.randomElement()!)
+        }
+    }
+    @IBAction func batsu(_ sender: Any)
+    {
+    for imageView in dices{
+    imageView.image =
+        UIImage(named:imageNames.randomElement()!)
+    }
 }
-
+    @IBAction func maru(_ sender: Any) {
+    for imageView in dices{
+    imageView.image =
+        UIImage(named:imageNames.randomElement()!)
+    }
+}
+    @IBAction func sehou(_ sender: Any) {
+    for imageView in dices{
+    imageView.image =
+        UIImage(named:imageNames.randomElement()!)
+    }
+}
+    @IBAction func showSwitch(_ sender: UISwitch) {
+        for imageView in dices{
+            imageView.isHidden = !sender.isOn
+        }
+    }
+}
